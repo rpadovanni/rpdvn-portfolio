203514badf15f19f031974851313e7eee2c8a7e6
Rafael Padovani <r.padovanni@hotmail.com>
2025-02-18 13:43:45 -0300

    New Version Reset

--- src/components/header/menu-item.tsx
+++
@@ -1,9 +0,0 @@
-type TMenuItemProps = {
-  children: React.ReactNode;
-};
-
-const MenuItem = ({ children }: TMenuItemProps) => {
-  return <li className="my-4 flex w-full justify-center text-pretty p-2 text-xl">{children}</li>;
-};
-
-export default MenuItem;

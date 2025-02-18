ca226af52eed100a35994027b8afd92b66edc282
Rafael Padovani <r.padovanni@hotmail.com>
2025-02-18 14:10:33 -0300

    feat: `astro.js` initial setup

---
+++ src/env.d.ts
@@ -0,0 +1,6 @@
+// Custom types declarations
+declare var myString: string;
+declare function myFunction(): boolean;
+
+// Astro types, not necessary if you already have a tsconfig.json
+/// <reference path="../.astro/types.d.ts" />
